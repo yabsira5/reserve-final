abb927e19a993663e412958f670d46ecc77de73c
zabs22 <zabsdere@gmail.com>
2023-01-15 11:33:01 +0300

    reserve edited

--- reservation-backend/client/reserve-web/src/components/reserve/Reserve.js
+++ reservation-backend/client/reserve-web/src/components/reserve/Reserve.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import './Reserve.css';
 import { useNavigate } from 'react-router-dom';
-import CloseIcon from '@mui/icons-material/Close';
 
 const Reserve = ({setOpen}) => {
     const navigate = useNavigate();
@@ -12,36 +11,45 @@ const Reserve = ({setOpen}) => {
        }
      };
   return (
-    <div>
-        <div className="reserve">
-            <div className="rContainer">
-                <CloseIcon 
-                    className="rClose"
-                    onClick={() => setOpen(false)}
-                />
-                <span>Select your rooms:</span>
-                <div className="rItem">
-                    <div className="rItemInfo">
-                        <div className="rTitle">Our Rooms</div>
-                        <div className="rDesc">Description</div>
-                        <div className="rMax">
-                        Max people: <b>2</b>
-                        </div>
-                        <div className="rPrice">ETB 130</div>
-                    </div>
-                    <div className="rSelectRooms">
-                        <div className="room">
-                            <label>Room Number:</label>
-                                <input
-                                    type="checkbox"
-                                />
-                        </div>
-                    </div>
+    <div className='reserve'>
+        <div className="roomContainer">
+            <img src="https://www.roadaffair.com/wp-content/uploads/2019/10/meyazia-27-square-addis-ababa-ethiopia-shutterstock_187152050.jpg"
+             alt="Image of the room" className="hero" />
+
+                <div className="summeryInfo container">
+                    <h2 className="summeryTitle">Booking Summary</h2>    
+                    <p className="summeryDescription">
+                        Here below is the room booking that you selected     
+                    </p> 
                 </div>
-                <button onClick={handleClick} className="rButton">
-                    Reserve Now!
-                </button>
+                <div className="roomContainer container">
+                    <div className="room">
+                        <div className="roomInfo">
+                             <h3 className="roomTitle">Room Title</h3>
+                            <p className="roomPrice">Addis hotel</p>
+                        </div>
+                        <div className="roomInfo">
+                             <h3 className="roomTitle">Room Price</h3>
+                            <p className="roomPrice">ETB 315</p>
+                        </div>
+                        <div className="roomInfo">
+                             <h3 className="roomTitle">Booked By</h3>
+                            <p className="roomPrice">Username</p>
+                        </div>
+                        <div className="roomInfo">
+                             <h3 className="roomTitle">Phone Number</h3>
+                            <p className="roomPrice">0911122234</p>
+                        </div>
+                </div> 
             </div>
+            
+                <button className="proceedBtn btn container" onClick={handleClick}> 
+                    Confirm by SMS     
+                </button>
+                <button className="cancelBtn btn container">  
+                    Cancel Booking        
+                </button>
+             
         </div>
     </div>
   )
